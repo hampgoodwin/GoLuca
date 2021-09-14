1b50de05bc2fef378f4fc935efaf3a207fe289fe
hampgoodwin <hampgoodwin@gmail.com>
2021-09-14 12:12:19 -0600

    implement descr and validation on entry; add fks; add index

--- pkg/transaction/types.go
+++ pkg/transaction/types.go
@@ -10,7 +10,7 @@ import (
 type Transaction struct {
 	ID          string    `json:"id" validate:"required,uuid4"`
 	Description string    `json:"description" validate:"required"`
-	Entries     []Entry   `json:"entries,omitempty" validate:"dive,gte=21"`
+	Entries     []Entry   `json:"entries,omitempty" validate:"dive,gte=1"`
 	CreatedAt   time.Time `json:"createdAt" validate:"required"`
 }
 
@@ -19,6 +19,7 @@ type Entry struct {
 	// Account account.Account `validate:"required"`
 	ID            string        `json:"id" validate:"required,uuid4"`
 	TransactionID string        `json:"transaction_id" validate:"required,uuid4"`
+	Description   string        `json:"description"`
 	CreditAccount string        `json:"creditAccount" validate:"required,uuid4"`
 	DebitAccount  string        `json:"debitAccount" validate:"required,uuid4"`
 	Amount        amount.Amount `json:"amount" validate:"required"`
