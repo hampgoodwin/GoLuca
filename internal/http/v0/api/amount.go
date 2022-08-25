0357a9dbe34091d8df441a5ef4751b828c2759b7
hampgoodwin <hampgoodwin@gmail.com>
2022-08-25 00:30:33 -0600

    remove api models as they're now public

--- internal/http/v0/api/amount.go
+++
@@ -1,6 +0,0 @@
-package api
-
-type Amount struct {
-	Value    string `json:"value" validate:"int64,gte=0"`
-	Currency string `json:"currency" validate:"len=3,alpha"`
-}
--- internal/http/v0/api/entry.go
+++
@@ -1,8 +0,0 @@
-package api
-
-type Entry struct {
-	Description   string `json:"description"`
-	DebitAccount  string `json:"debitAccount" validate:"required,uuid4"`
-	CreditAccount string `json:"creditAccount" validate:"required,uuid4"`
-	Amount        Amount `json:"amount" validate:"required"`
-}
