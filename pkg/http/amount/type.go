49c6da590c4a4c4b7ea970b378a17ca9f0b560a5
hampgoodwin <hampgoodwin@gmail.com>
2022-08-24 23:46:41 -0600

    add transaction types to support http interface

---
+++ pkg/http/amount/type.go
@@ -0,0 +1,6 @@
+package amount
+
+type Amount struct {
+	Value    string `json:"value" validate:"int64,gte=0"`
+	Currency string `json:"currency" validate:"len=3,alpha"`
+}
---
+++ pkg/http/transaction/type.go
@@ -0,0 +1,48 @@
+package transaction
+
+import (
+	"time"
+
+	httpamount "github.com/hampgoodwin/GoLuca/pkg/http/amount"
+)
+
+type CreateTransaction struct {
+	Description string  `json:"description" validate:"required"`
+	Entries     []Entry `json:"entries,omitempty" validate:"dive,gte=1"`
+}
+
+func (t Transaction) IsZero() bool {
+	if t.Description != "" {
+		return false
+	}
+	if t.Entries != nil {
+		return false
+	}
+	return true
+}
+
+type CreateEntry struct {
+	Description   string            `json:"description"`
+	DebitAccount  string            `json:"debitAccount" validate:"required,uuid4"`
+	CreditAccount string            `json:"creditAccount" validate:"required,uuid4"`
+	Amount        httpamount.Amount `json:"amount" validate:"required"`
+}
+
+// Transaction ...
+type Transaction struct {
+	ID          string    `json:"id" validate:"required,uuid4"`
+	Description string    `json:"description" validate:"required"`
+	Entries     []Entry   `json:"entries,omitempty" validate:"dive,gte=1"`
+	CreatedAt   time.Time `json:"createdAt" validate:"required"`
+}
+
+// Entry ...
+type Entry struct {
+	ID            string            `json:"id" validate:"required,uuid4"`
+	TransactionID string            `json:"transaction_id" validate:"required,uuid4"`
+	Description   string            `json:"description"`
+	DebitAccount  string            `json:"debitAccount" validate:"required,uuid4"`
+	CreditAccount string            `json:"creditAccount" validate:"required,uuid4"`
+	Amount        httpamount.Amount `json:"amount" validate:"required"`
+	CreatedAt     time.Time         `json:"createdAt" validate:"required"`
+}
