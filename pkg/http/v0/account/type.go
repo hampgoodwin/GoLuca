43fb799c193c3f080732093c3d83453cb7804bb3
hampgoodwin <hampgoodwin@gmail.com>
2022-08-25 18:52:25 -0600

    update request definitions in openapi.yml spec

--- pkg/http/v0/account/type.go
+++ pkg/http/v0/account/type.go
@@ -3,7 +3,7 @@ package account
 import "github.com/hampgoodwin/GoLuca/pkg/account"
 
 type CreateAccount struct {
-	ParentID string       `json:"parentId" validate:"omitempty,uuid4"`
+	ParentID string       `json:"parentId,omitempty" validate:"omitempty,uuid4"`
 	Name     string       `json:"name" validate:"required"`
 	Type     account.Type `json:"type" validate:"required,oneof=asset liability equity revenue expense gain loss"`
 	Basis    string       `json:"basis" validate:"required,oneof=debit credit"`
