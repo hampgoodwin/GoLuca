f39d879de96bd7ed9f003583b0a8007d2270badf
hampgoodwin <hampgoodwin@gmail.com>
2022-08-25 23:54:32 -0600

    fix ksuid<-uuid merge issue

--- pkg/http/v0/account/type.go
+++ pkg/http/v0/account/type.go
@@ -3,15 +3,15 @@ package account
 import "time"
 
 type CreateAccount struct {
-	ParentID string `json:"parentId,omitempty" validate:"omitempty,uuid4"`
+	ParentID string `json:"parentId,omitempty" validate:"omitempty,KSUID"`
 	Name     string `json:"name" validate:"required"`
 	Type     string `json:"type" validate:"required,oneof=asset liability equity revenue expense gain loss"`
 	Basis    string `json:"basis" validate:"required,oneof=debit credit"`
 }
 
 type Account struct {
-	ID        string    `json:"id" validate:"required,uuid4"`
-	ParentID  string    `json:"parentId,omitempty" validate:"omitempty,uuid4"`
+	ID        string    `json:"id" validate:"required,KSUID"`
+	ParentID  string    `json:"parentId,omitempty" validate:"omitempty,KSUID"`
 	Name      string    `json:"name" validate:"required"`
 	Type      string    `json:"type" validate:"required"`
 	Basis     string    `json:"basis" validate:"required,oneof=debit credit"`
--- pkg/transaction/types.go
+++ pkg/transaction/types.go
@@ -8,7 +8,7 @@ import (
 
 // Transaction ...
 type Transaction struct {
-	ID          string    `validate:"required,uuid4"`
+	ID          string    `validate:"required,KSUID"`
 	Description string    `validate:"required"`
 	Entries     []Entry   `validate:"dive,gte=1"`
 	CreatedAt   time.Time `validate:"required"`
@@ -16,11 +16,11 @@ type Transaction struct {
 
 // Entry ...
 type Entry struct {
-	ID            string `validate:"required,uuid4"`
-	TransactionID string `validate:"required,uuid4"`
+	ID            string `validate:"required,KSUID"`
+	TransactionID string `validate:"required,KSUID"`
 	Description   string
-	DebitAccount  string        `validate:"required,uuid4"`
-	CreditAccount string        `validate:"required,uuid4"`
+	DebitAccount  string        `validate:"required,KSUID"`
+	CreditAccount string        `validate:"required,KSUID"`
 	Amount        amount.Amount `validate:"required"`
 	CreatedAt     time.Time     `validate:"required"`
 }
