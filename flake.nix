e05a9ff586091bc93adb3caf0ea6c5406110b0bf
Hamp Goodwin <hampgoodwin@gmail.com>
2025-08-04 08:50:24 -0600

    moar nix develop

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Goluca App";
 
   inputs = {
-    inputs.flake-utils.url = "github:numtide/flake-utils?ref=1.0.0"; # I'm not sure if this is proper tag ref
+    flake-utils.url = "github:numtide/flake-utils?ref=v1.0.0"; # I'm not sure if this is proper tag ref
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.05";
   };
 
@@ -12,10 +12,21 @@
       nixpkgs,
       flake-utils,
     }:
-    flake-utils.lib.eachDefaultSystem(system:
-    let pkgs = nixpkgs.legacyPackages.${system};
-    in {
-        devShells.goluca = pkgs.mkShell { buildInput  = [pkgs.go_1_24 pkgs.gopls pkgs.delve pkgs.gofumpt pkgs.golangci-lint ];};
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.goluca = pkgs.mkShell {
+          buildInput = [
+            pkgs.go_1_24
+            pkgs.gopls
+            pkgs.delve
+            pkgs.gofumpt
+            pkgs.golangci-lint
+          ];
+        };
       }
-  )
+    );
 }
